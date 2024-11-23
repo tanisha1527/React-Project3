0158d43aa212e57e13d9c61edebe7d267204d227
tanisha1527 <vermatanisha620@gmail.com>
2024-11-24 01:16:05 +0530

    displayed data in table

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import  ReactDOM  from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import CoinContextProvider from './context/CoinContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <CoinContextProvider>
+       <App />
+    </CoinContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
+
