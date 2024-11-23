dddd1d75bdfed1d4c64f2dddb8c02202c8d1fce9
tanisha1527 <vermatanisha620@gmail.com>
2024-11-23 23:29:19 +0530

    create new folders and files

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <div className="hero">
+           <h1>Largest <br/> Crypto Marketplace</h1>
+           <p>Welcome to the world's largest cryptocurrency marketplace. Sign up to explore more about cryptos.</p>
+           <form>
+             <input type="text" placeholder='Search crypto...'/>
+             <button type='submit'>Search</button>
+           </form>
+        </div>
+    </div>
+  )
+}
+
+export default Home
