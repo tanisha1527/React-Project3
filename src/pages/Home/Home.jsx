f025afc2419416b03383b9fe74a1f2fa00d721e3
tanisha1527 <vermatanisha620@gmail.com>
2024-11-23 23:38:20 +0530

    create table header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,6 +12,15 @@ const Home = () => {
              <button type='submit'>Search</button>
            </form>
         </div>
+        <div className="crypto-table">
+           <div className="table-layout">
+              <p>#</p>
+              <p>Coins</p>
+              <p>Price</p>
+              <p style={{textAlign:"center"}}>24H Change</p>
+              <p className='market-cap'>Market Cap</p>
+           </div>
+        </div>
     </div>
   )
 }
