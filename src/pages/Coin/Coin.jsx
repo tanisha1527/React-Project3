c0f506220768db4459d2585812ffceb26c00e91b
tanisha1527 <vermatanisha620@gmail.com>
2024-11-25 18:02:56 +0530

    added additional coin-info on coin page

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -50,6 +50,30 @@ const Coin = () => {
           <div className="coin-chart">
              <LineChart historicalData={historicalData} />
           </div>
+     
+         <div className="coin-info">
+             <ul>
+                <li>Crypto Market Rank</li>
+                <li>{coinData.market_cap_rank}</li>
+             </ul>
+             <ul>
+                <li>Current Price</li>
+                <li>{currency.symbol} {coinData.market_data.current_price[currency.name].toLocaleString()}</li>
+             </ul>
+             <ul>
+                <li>Market Cap</li>
+                <li>{currency.symbol} {coinData.market_data.market_cap[currency.name].toLocaleString()}</li>
+             </ul>
+             <ul>
+                <li>24 Hour High</li>
+                <li>{currency.symbol} {coinData.market_data.high_24h[currency.name].toLocaleString()}</li>
+             </ul>
+             <ul>
+                <li>24 Hour Low</li>
+                <li>{currency.symbol} {coinData.market_data.low_24h[currency.name].toLocaleString()}</li>
+             </ul>
+         </div>
+
       </div>
     )  
   }else{
