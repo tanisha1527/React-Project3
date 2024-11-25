3b3ae0294cee5f862410844fc69f9461f3e2a7d1
tanisha1527 <vermatanisha620@gmail.com>
2024-11-25 13:33:56 +0530

    add search functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import './Navbar.css'
 import logo from '../../assets/logo.png'
 import arrow_icon from '../../assets/arrow_icon.png'
 import { CoinContext } from '../../context/CoinContext'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -32,9 +33,11 @@ const Navbar = () => {
 
   return (
     <div className='navbar'>
+       <Link to={'/'}>
          <img src={logo} alt="" className='logo' />
+         </Link>
          <ul>
-             <li>Home</li>
+         <Link to={'/'}><li>Home</li></Link>
              <li>Features</li>
              <li>Pricing</li>
              <li>Blog</li>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,29 @@
 import React, { useContext, useEffect, useState } from 'react'
 import './Home.css'
 import { CoinContext } from '../../context/CoinContext'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 
   const {allCoin, currency} = useContext(CoinContext);
   const [displayCoin, setDisplayCoin] = useState([]);
+  const [input, setInput] = useState("");
+  
+
+  const inputHandler = (event)=>{
+       setInput(event.target.value);
+       if(event.target.value === ""){
+          setDisplayCoin(allCoin);
+       }
+  }
+
+  const searchHandler = async (event)=>{
+        event.preventDefault();
+        const coins = await allCoin.filter((item)=>{
+           return item.name.toLowerCase().includes(input.toLocaleLowerCase())
+        })
+        setDisplayCoin(coins);
+}
 
   useEffect(()=>{
       setDisplayCoin(allCoin);
@@ -16,8 +34,13 @@ const Home = () => {
         <div className="hero">
            <h1>Largest <br/> Crypto Marketplace</h1>
            <p>Welcome to the world's largest cryptocurrency marketplace. Sign up to explore more about cryptos.</p>
-           <form>
-             <input type="text" placeholder='Search crypto...'/>
+           <form onSubmit={searchHandler}>
+             <input onChange={inputHandler} list='coinlist' value={input} type="text" placeholder='Search crypto...' required/>
+
+            <datalist id='coinlist'>
+                 {allCoin.map((item, index)=>(<option key={index} value={item.name}/>))}
+            </datalist>
+
              <button type='submit'>Search</button>
            </form>
         </div>
@@ -31,7 +54,7 @@ const Home = () => {
            </div>
             {
               displayCoin.slice(0,10).map((item, index)=>(
-                 <div className="table-layout" key={index}>
+                 <Link to={`/coin/${item.id}`} className="table-layout" key={index}>
                      <p>{item.market_cap_rank}</p>
                      <div>
                         <img src={item.image} alt="" />
@@ -41,7 +64,7 @@ const Home = () => {
                      <p className={item.price_change_percentage_24h>0?"green":"red"}>
                       {Math.floor(item.price_change_percentage_24h*100)/100}</p>
                      <p className='market-cap'>{currency.symbol} {item.market_cap.toLocaleString()}</p>
-                 </div>
+                 </Link>
               ))
             }
         </div>
