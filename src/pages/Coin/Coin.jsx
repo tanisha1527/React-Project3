58212352608d512b01f2c0bdc5b4b17bc1f1f4ee
tanisha1527 <vermatanisha620@gmail.com>
2024-11-25 14:19:17 +0530

    added API data fetching functionality

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,12 +1,61 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Coin.css'
+import { useParams } from 'react-router-dom'
+import { CoinContext } from '../../context/CoinContext';
 
 const Coin = () => {
-  return (
-    <div>
-       <h2>Coin</h2>
-    </div>
-  )
+
+  const {coinId} = useParams();
+  const [coinData, setCoinData] = useState();
+  const [historicalData, setHistoricalData] = useState();
+  const {currency} = useContext(CoinContext);
+
+  const fetchCoinData = async ()=>{
+    const options = {
+      method: 'GET',
+      headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-JbnXTotEKX7TkJbJ4udYZKMF'}
+    };
+    
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then(res => res.json())
+      .then(res => setCoinData(res))
+      .catch(err => console.error(err));
+  }
+
+  const fetchHistoricalData =async ()=>{
+    const options = {
+      method: 'GET',
+      headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-JbnXTotEKX7TkJbJ4udYZKMF'}
+    };
+    
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10`, options)
+      .then(res => res.json())
+      .then(res => setHistoricalData(res))
+      .catch(err => console.error(err));
+  }
+
+  useEffect(()=>{
+     fetchCoinData();
+  },[currency])
+  
+  if(coinData, historicalData){
+    return (
+      <div className="coin">
+          <div className="coin-name">
+             <img src={coinData.image.large} alt="" />
+             <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+          </div>
+      </div>
+    )  
+  }else{
+    return (
+      <div className="spinner">
+          <div className="spin">
+             
+          </div>
+      </div>
+    )
+  }
 }
 
 export default Coin
